e6357fd07cc04442956fbbf6c9511410ca15a297
Robayed110 <126972094+Robayed110@users.noreply.github.com>
2023-06-09 02:41:52 +0600

    SplashScreen,ResetPassword Creation and Database Connection

--- app/src/main/java/com/example/cardiacrecorder/MainActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://cardiacrecorder-eb8d3-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/cardiacrecorder/TestLogin.java
@@ -0,0 +1,38 @@
+package com.example.cardiacrecorder;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestLogin extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=300;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_login);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String s1 = null;
+                SharedPreferences sharedPreferences=getSharedPreferences(Login.PREFS_NAME,0);
+                SharedPreferences sharedPreferences1=getSharedPreferences(Login.PREFS_NAME,0);
+                String hasLoggedIn2= sharedPreferences.getString("hasLoggedIn2",s1);
+                boolean hasLoggedIn=sharedPreferences.getBoolean("hasLoggedIn",false);
+                if(hasLoggedIn){
+                    Intent intent=new Intent(TestLogin.this,MainActivity.class);
+                    intent.putExtra("var",hasLoggedIn2);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Intent intent=new Intent(TestLogin.this,Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
